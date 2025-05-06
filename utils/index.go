10b5dcead6f5d07b1276dec22d1f4c2a2e39baf7
Joel Calderon <joel8080ur@gmail.com>
2025-05-06 14:20:11 -0600

    add new utils

---
+++ utils/index.go
@@ -0,0 +1,27 @@
+package utils
+
+import "time"
+
+func Pstring(str string) *string {
+	return &str
+}
+
+func Pbool(boolean bool) *bool {
+	return &boolean
+}
+
+func Pint(integer int64) *int64 {
+	return &integer
+}
+
+func Pfloat(float float64) *float64 {
+	return &float
+}
+
+func PTimeDuration(timeDuration time.Duration) *time.Duration {
+	return &timeDuration
+}
+
+func PTime(time time.Time) *time.Time {
+	return &time
+}
